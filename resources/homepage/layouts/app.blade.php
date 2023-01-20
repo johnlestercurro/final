7d974c2b2f79086681b8fcb598e97c8ac5c55af3
johnlestercurro <johnlestercurro@gmail.com>
2023-01-20 16:49:53 +0800

    Added Template & Exploring blade

--- resources/homepage/layouts/app.blade.php
+++ resources/homepage/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Homepage</title>
+        <title>Modern Business - Start Bootstrap Template</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
@@ -15,9 +15,9 @@
     </head>
     <body class="d-flex flex-column h-100">
         <main class="flex-shrink-0">
+
             @yield('content')
-           
-            <!-- Bootstrap core JS-->
+        <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
